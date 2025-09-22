6d9c19523f9ba4ecd8be0eab19e6d9f17a220722
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-09-22 17:25:07 +0200

    can get the token

---
+++ Sources/init.py
@@ -0,0 +1,15 @@
+import	os
+from	dotenv		import load_dotenv
+
+def load_env():
+	load_dotenv()
+	discord_token = os.getenv("DISCORD_TOKEN")
+	if not discord_token:
+		raise ValueError("DISCORD_TOKEN not found")
+	ft_uid = os.getenv("42_UID")
+	if not ft_uid:
+		raise ValueError("42_UID not found")
+	ft_secret = os.getenv("42_SECRET")
+	if not ft_secret:
+		raise ValueError("42_SECRET not found")
+	return (discord_token, ft_uid, ft_secret)
--- Sources/main.py
+++ Sources/main.py
@@ -1,7 +1,12 @@
-import	os
 import	discord
+import	requests
 from 	discord.ext	import commands
-from	dotenv		import load_dotenv
+from	init		import load_env
+
+ft_uid = None
+ft_secret = None
+ft_token_access = None
+ft_token_exp = 0
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,12 +16,30 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
 	await bot.change_presence(status=discord.Status.online, activity=discord.Game('Work in progress'))
 
+def get_access_token():
+	global ft_token_exp, ft_token_access
+	if (ft_token_access or ft_token_exp < 30):
+		request = requests.post(
+			"https://api.intra.42.fr/oauth/token",
+			data={
+				"grant_type": "client_credentials",
+				"client_id": ft_uid,
+				"client_secret": ft_secret
+			},
+			timeout=15
+		)
+		request.raise_for_status()
+		ft_token_access = request.json()["access_token"]
+		ft_token_exp = request.json()["expires_in"]
+
+
+
 def main():
-	load_dotenv()
-	token = os.getenv("DISCORD_TOKEN")
-	if not token:
-		raise ValueError("DISCORD_TOKEN not found")
-	bot.run(token)
+	global ft_uid, ft_secret
+
+	discord_token, ft_uid, ft_secret = load_env()
+	get_access_token()
+	bot.run(discord_token)
 
 if __name__ == "__main__":
 	main()
